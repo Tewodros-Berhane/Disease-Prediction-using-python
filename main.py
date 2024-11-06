4515e71d2c34b56ec50c36d6b0b556349f46891e
Tewodros-Berhane <163821121+Tewodros-Berhane@users.noreply.github.com>
2024-11-06 18:58:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from sklearn.tree import DecisionTreeClassifier,_tree
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVC
 import csv
 import warnings
 
