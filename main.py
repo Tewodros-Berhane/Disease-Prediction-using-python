12e885188d4154e051edcb2cae863cd113bf92a9
Tewodros <tewodrosberhaneabraha@gmail.com>
2024-11-06 18:49:55 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,297 @@
+import re
+import os
+import json
+import uuid
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier,_tree
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+import csv
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+training = pd.read_csv('Data/Training.csv')
+testing= pd.read_csv('Data/Testing.csv')
+cols= training.columns
+cols= cols[:-1]
+x = training[cols]
+y = training['prognosis']
+y1= y
+
+
+reduced_data = training.groupby(training['prognosis']).max()
+
+#mapping strings to numbers
+le = preprocessing.LabelEncoder()
+le.fit(y)
+y = le.transform(y)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+testx    = testing[cols]
+testy    = testing['prognosis']  
+testy    = le.transform(testy)
+
+
+clf1  = DecisionTreeClassifier()
+clf = clf1.fit(x_train,y_train)
+scores = cross_val_score(clf, x_test, y_test, cv=3)
+print (scores.mean())
+
+
+importances = clf.feature_importances_
+indices = np.argsort(importances)[::-1]
+features = cols
+
+
+severityDictionary=dict()
+description_list = dict()
+precautionDictionary=dict()
+
+symptoms_dict = {}
+
+for index, symptom in enumerate(x):
+       symptoms_dict[symptom] = index
+def calc_condition(exp,days):
+    sum=0
+    for item in exp:
+         sum=sum+severityDictionary[item]
+    if((sum*days)/(len(exp)+1)>13):
+        print("You should take the consultation from doctor. ")
+    else:
+        print("It might not be that bad but you should take precautions.")
+
+
+def getDescription():
+    global description_list
+    with open('MasterData/symptom_Description.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            _description={row[0]:row[1]}
+            description_list.update(_description)
+
+
+
+
+def getSeverityDict():
+    global severityDictionary
+    with open('MasterData/symptom_severity.csv') as csv_file:
+
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        try:
+            for row in csv_reader:
+                _diction={row[0]:int(row[1])}
+                severityDictionary.update(_diction)
+        except:
+            pass
+
+
+def getprecautionDict():
+    global precautionDictionary
+    with open('MasterData/symptom_precaution.csv') as csv_file:
+
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            _prec={row[0]:[row[1],row[2],row[3],row[4]]}
+            precautionDictionary.update(_prec)
+
+def load_user_data():
+    if not os.path.exists('user_data.json'):
+        with open('user_data.json', 'w') as f:
+            json.dump([], f)
+    with open('user_data.json', 'r') as f:
+        return json.load(f)
+
+def save_user_data(data):
+    with open('user_data.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def getInfo():
+    global user_id, diagnosis_history
+    user_data = load_user_data()
+    
+    print("-----------------------------------Disease Prediction-----------------------------------")
+    print("Your Name", end=": ")
+    name = input("")
+    
+    existing_user = next((user for user in user_data if user['name'] == name), None)
+    
+    if existing_user:
+        print("Hello again,", existing_user['name'])
+        user_id = existing_user['id']
+        diagnosis_history = existing_user['diagnosis_history']
+    else:
+        age = input("Your Age: ")
+        user_id = str(uuid.uuid4())
+        new_user = {
+            'id': user_id,
+            'name': name,
+            'age': age,
+            'diagnosis_history': []
+        }
+        user_data.append(new_user)
+        save_user_data(user_data)
+        print("Hello,", name)
+        diagnosis_history = new_user['diagnosis_history']
+
+def check_pattern(dis_list,inp):
+    pred_list=[]
+    inp=inp.replace(' ','_')
+    patt = f"{inp}"
+    regexp = re.compile(patt)
+    pred_list=[item for item in dis_list if regexp.search(item)]
+    if(len(pred_list)>0):
+        return 1,pred_list
+    else:
+        return 0,[]
+    
+def sec_predict(symptoms_exp):
+    df = pd.read_csv('Data/Training.csv')
+    X = df.iloc[:, :-1]
+    y = df['prognosis']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
+    rf_clf = DecisionTreeClassifier()
+    rf_clf.fit(X_train, y_train)
+
+    symptoms_dict = {symptom: index for index, symptom in enumerate(X)}
+    input_vector = np.zeros(len(symptoms_dict))
+    for item in symptoms_exp:
+        input_vector[symptoms_dict[item]] = 1
+
+    input_df = pd.DataFrame([input_vector], columns=symptoms_dict.keys())
+    return rf_clf.predict(input_df)
+
+
+def print_disease(node):
+    node = node[0]
+    val  = node.nonzero() 
+    disease = le.inverse_transform(val[0])
+    return list(map(lambda x:x.strip(),list(disease)))
+
+
+def update_diagnosis_history(user_id, diagnosis):
+    user_data = load_user_data()
+    for user in user_data:
+        if user['id'] == user_id:
+            user['diagnosis_history'].append(diagnosis)
+            break
+    save_user_data(user_data)
+
+
+def tree_to_code(tree, feature_names):
+    tree_ = tree.tree_
+    feature_name = [
+        feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
+        for i in tree_.feature
+    ]
+
+    chk_dis=",".join(feature_names).split(",")
+    symptoms_present = []
+
+    while True:
+        print("=====================================================================")
+        print('\n')
+        print("Enter the symptom you are experiencing",end=": ")
+        disease_input = input("")
+        conf,cnf_dis=check_pattern(chk_dis,disease_input)
+        if conf==1:
+            print("searches related to input: ")
+            for num,it in enumerate(cnf_dis):
+                print(num,")",it)
+            if num!=0:
+                print(f"Select the one you meant (0 - {num}):  ", end="")
+                conf_inp = int(input(""))
+            else:
+                conf_inp=0
+
+            disease_input=cnf_dis[conf_inp]
+            break
+        else:
+            print("Enter valid symptom.")
+
+    while True:
+        try:
+            num_days=int(input("Okay. From how many days ? : "))
+            break
+        except:
+            print("Enter valid input.")
+    def recurse(node, depth):
+        indent = "  " * depth
+        if tree_.feature[node] != _tree.TREE_UNDEFINED:
+            name = feature_name[node]
+            threshold = tree_.threshold[node]
+
+            if name == disease_input:
+                val = 1
+            else:
+                val = 0
+            if  val <= threshold:
+                recurse(tree_.children_left[node], depth + 1)
+            else:
+                symptoms_present.append(name)
+                recurse(tree_.children_right[node], depth + 1)
+        else:
+            present_disease = print_disease(tree_.value[node])
+            red_cols = reduced_data.columns 
+            symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
+
+            print("=====================================================================")
+            print('\n')
+            print("Are you experiencing any ")
+            symptoms_exp=[]
+            for syms in list(symptoms_given):
+                inp=""
+                print(syms,"? : ",end='')
+                while True:
+                    inp=input("")
+                    if(inp=="yes" or inp=="no"):
+                        break
+                    else:
+                        print("provide proper answers i.e. (yes/no) : ",end="")
+                if(inp=="yes"):
+                    symptoms_exp.append(syms)
+            print("=====================================================================")
+            print('\n')
+            second_prediction=sec_predict(symptoms_exp)
+            calc_condition(symptoms_exp,num_days)
+            if(present_disease[0]==second_prediction[0]):
+                print("You may have ", present_disease[0])
+                print("=====================================================================")
+                print('\n')
+                print(description_list[present_disease[0]])
+                update_diagnosis_history(user_id, present_disease[0])
+
+
+            else:
+                print("You may have ", present_disease[0], "or ", second_prediction[0])
+                print("=====================================================================")
+                print('\n')
+                print(description_list[present_disease[0]])
+                print(description_list[second_prediction[0]])
+                update_diagnosis_history(user_id, present_disease[0])
+
+            precution_list=precautionDictionary[present_disease[0]]
+            print("=====================================================================")
+            print('\n')
+            print("Take following measures : ")
+            for  i,j in enumerate(precution_list):
+                print(i+1,")",j)
+
+
+    recurse(0, 1)
+getSeverityDict()
+getDescription()
+getprecautionDict()
+getInfo()
+tree_to_code(clf,cols)
+print("----------------------------------------------------------------------------------------")
+
